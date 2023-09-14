e7926c9a219f27f89dac3bd21205f12958898b32
Alan Medina <alanmedc98@gmail.com>
2023-09-14 10:31:51 -0600

    campo id_grupo agragado

--- src/controllers/alumno.controller.ts
+++ src/controllers/alumno.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 
 export const crearSolicitud = async (req: Request, res: Response) => {
-    const {expediente, nombre_alumno, ap_paterno, ap_materno, email_alumno} = req.body
+    const {expediente_alumno, nombre_alumno, ap_paterno, ap_materno, email_alumno, id_grupo} = req.body
     try {
-        const [rows] = await pool.query('INSERT INTO Solicitud (expediente, nombre_alumno, ap_paterno, ap_materno, email_alumno) VALUES (?, ?, ?, ?, ?)'
-        , [expediente, nombre_alumno, ap_paterno, ap_materno, email_alumno])
+        const [rows] = await pool.query('INSERT INTO Solicitud (expediente, nombre_alumno, ap_paterno, ap_materno, email_alumno, id_grupo) VALUES (?, ?, ?, ?, ?, ?)'
+        , [expediente_alumno, nombre_alumno, ap_paterno, ap_materno, email_alumno, id_grupo])
         res.send('Solicitud creada correctamente.')
         res.json(rows[0])
     } catch (error) {
